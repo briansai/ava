406c662022c3b1c5f980c0d953913c761bf0066b
Brian Sai <brianwsai@gmail.com>
2021-01-29 17:40:26 -0800

    Initial setup for application

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+  // create info page
+  // get information from backend info endpoint
+  // create button to enter app
+  // create users
+  // create main box component
+  // create screen for all conversations
+  // create text box
+  // create popup on click conversation
+  // display graph of edits
+  // mutate text box
+  // mutation history
+  return <div className="App"></div>;
+};
+
+export default App;
